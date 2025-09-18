9587d99f8140e7314869f6792086ec823a9863ca
niyagriff200 <130501997+niyagriff200@users.noreply.github.com>
2025-09-18 14:14:34 -0500

    Smaller Updates, (adding comments, debug.log, etc.)

--- Assets/Scripts/QuitGame.cs
+++ Assets/Scripts/QuitGame.cs
@@ -13,6 +13,7 @@ public class QuitGame : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
+            Debug.Log("The player has quit the game.");
             //When the player pushes down on esc key, they will quit the application
             Application.Quit();
         }
